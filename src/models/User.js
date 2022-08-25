061a816740ea3be636ea2e1f7950e9d094346f44
rimehri <rimeh.berrichi@esprit.tn>
2022-08-25 21:31:02 +0100

    register

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    Name: {type: String, require: false},
-     gender: {type: String, require: false,},
-    add: {type: String, require: false,},
-    
-  
-    phone: {type: Number},
-    email: {type: String, require: false, unique: true, lowercase: true},
-    password: {type: String, require: false},
-    image: {type: String, default: "avatar.jpeg"},
-    resetCode :{type:Number,require: false},
-    isActive: {type: Boolean, default: false},
-   
+  _id: mongoose.Schema.Types.ObjectId,
+  Name: { type: String, require: false },
+  gender: { type: String, require: false },
+  add: { type: String, require: false },
 
-
+  phone: { type: Number },
+  email: { type: String, require: false, unique: true, lowercase: true },
+  password: { type: String, require: false },
+  image: { type: String, default: "avatar.jpeg" },
+  resetCode: { type: Number, require: false },
+  isActive: { type: Boolean, default: false },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
